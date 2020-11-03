d4b3929d00e3c9f27e6259524dea7b39365a70c5
40397559 <40397559@live.napier.ac.uk>
2020-11-03 14:26:50 +0000

    Add project files.

---
+++ SET09122/AStar.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SET09122
+{
+    class AStar
+    {
+    }
+}
---
+++ SET09122/ImportData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SET09122
+{
+    class ImportData
+    {
+
+    }
+}
